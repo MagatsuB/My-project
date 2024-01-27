6aaeb01d8de271159b0dd52f7d094bd027bd2b2f
Breno E S L Reis <brereis@outlook.com>
2024-01-26 23:18:07 -0300

    Projeto teste com NavMesh

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    [SerializeField] Transform _target;
+
+    NavMeshAgent _agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _agent.updateRotation = false;
+        _agent.updateUpAxis = false;
+        
+    }
+
+    // Update is called once per frame
+   private void Update()
+    {
+        _agent.SetDestination(_target.position);
+    }
+}
